c492d2be81bd4653ad35e2d6b707099a57706794
samirgouda64 <159049183+samirgouda64@users.noreply.github.com>
2025-06-21 10:49:16 +0530

    Update AuthContext.jsx

--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -3,8 +3,8 @@ import React, { createContext, useState } from "react";
 export const authDataContext = createContext();
 
 function AuthContext({ children }) {
-  // let serverUrl = "https://wanderlust-backend-1d10.onrender.com";
-  let serverUrl = "http://localhost:8000";
+  let serverUrl = "https://wanderlust-backend-1d10.onrender.com";
+  // let serverUrl = "http://localhost:8000";
 
   let [loading, setLoading] = useState(false);
 
