22bf07047b31c33a2e62bcbde4b6ef5285daf264
Samirgouda64 <samirgouda439@gmail.com>
2025-06-21 10:46:16 +0530

    change the serverUrl

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://wanderlust-frontend-4oz6.onrender.com",
+    // origin: "https://wanderlust-frontend-4oz6.onrender.com",
+    origin: "http://localhost:5173",
     credentials:true
   })
 );
