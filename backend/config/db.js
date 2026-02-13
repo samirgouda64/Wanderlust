1e3113647af45b4fed14cee57b4962426aa17274
Samirgouda64 <samirgouda439@gmail.com>
2026-02-13 15:40:02 +0530

    Change enviroment variable

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL_PROD);
+        await mongoose.connect(process.env.MONGODB_URL);
         console.log("DB connected");
     } catch (error) {
         console.log("db error");
